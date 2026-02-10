9c1a80c6c6b188473dc959b3c2674a434cfd5087
ImCodeMaker <xxjoshi2006@gmail.com>
2026-02-09 23:57:40 -0400

    fixessss

--- api-gateway/src/common/logger/logger.module.ts
+++ api-gateway/src/common/logger/logger.module.ts
@@ -1,6 +1,12 @@
-import { Module, Global } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { LoggerService } from './logger.service';
 
+/**
+ * Módulo global de logging
+ * 
+ * @Global() hace que LoggerService esté disponible en TODOS los módulos
+ * sin necesidad de importar LoggerModule en cada uno
+ */
 @Global()
 @Module({
   providers: [LoggerService],
