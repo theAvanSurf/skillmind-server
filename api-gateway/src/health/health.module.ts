a17048d642383da13d0a8260978c20c24f0ef482
jorgematos27 <96342041+jorgematos27@users.noreply.github.com>
2026-03-10 16:14:03 -0400

    feat(SDT-21): implement global error handling, logging and traceability

--- api-gateway/src/health/health.module.ts
+++ api-gateway/src/health/health.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { ApiHealthController } from "./health.controller";
+import { RedisModule } from "../infrastucture/cache/redis.module";
+import { KafkaModule } from "../infrastucture/messaging/kafka.module";
 
 @Module({
+    imports: [RedisModule, KafkaModule],
     controllers: [ApiHealthController]
 })
-
 export class HealthModule {}
